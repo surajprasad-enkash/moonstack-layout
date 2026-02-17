3c341d4881012d99af9b260c2e719e0b40443f51
rskumawat0143 <ramprasadkumawat75@gmail.com>
2026-02-17 10:05:57 +0530

    code marge

--- src/components/ServicesSvg/HeaderSvgIcons/ReactIcon.tsx
+++ src/components/ServicesSvg/HeaderSvgIcons/ReactIcon.tsx
@@ -1,4 +1,4 @@
-export default function React(props: React.SVGProps<SVGSVGElement>) {
+export default function ReactIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
