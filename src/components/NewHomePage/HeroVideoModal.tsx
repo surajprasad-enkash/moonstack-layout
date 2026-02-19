38bd4120d5d6821d6da3e1871987b215f4071706
rskumawat0143 <ramprasadkumawat75@gmail.com>
2026-02-19 13:25:01 +0530

    code marge

--- src/components/NewHomePage/HeroVideoModal.tsx
+++ src/components/NewHomePage/HeroVideoModal.tsx
@@ -26,20 +26,22 @@ export default function HeroVideoModal({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
-      <button
-        onClick={onClose}
-        className="absolute top-6 right-6 cursor-pointer text-4xl font-light text-white"
-      >
-        ×
-      </button>
+      <div className="relative w-full max-w-[900px] px-[40px]">
+        <button
+          onClick={onClose}
+          className="absolute top-[-50px] right-[0px] cursor-pointer text-4xl font-light text-white"
+        >
+          ×
+        </button>
 
-      <div className="relative mx-4 aspect-video w-full max-w-4xl overflow-hidden rounded-lg bg-black">
-        <iframe
-          className="h-full w-full"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-        />
+        <div className="relative aspect-video w-full max-w-4xl overflow-hidden rounded-lg bg-black">
+          <iframe
+            className="h-full w-full"
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+        </div>
       </div>
     </div>
   )
